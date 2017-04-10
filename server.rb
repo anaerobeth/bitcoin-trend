93f93f24d4f036bfbe68bb6419dbdb4523d5ddda
Beth Tenorio <betsky98@gmail.com>
2017-04-09 23:39:42 -0400

    refactor server code

--- server.rb
+++ server.rb
@@ -3,23 +3,29 @@ require 'json'
 require 'net/http'
 
 post '/' do
-  # Connect to a server that retrieves Google Trend data for 'Bitcoin'
-  message = Net::HTTP.get(URI.parse("http://localhost:4000"))
-  timeline = JSON.parse(message)["default"]["timelineData"]
-  yesterday = timeline[-1]["value"][0]
-  last_week = timeline[0]["value"][0]
-  difference = yesterday - last_week
-  message = case difference
+  timeline = JSON.parse(data)["default"]["timelineData"]
+  message = case trend_diff(timeline)
             when > 5:
               'More '
             when < -5:
               'Fewer '
             else:
-              'The same number of'
+              'The same number of '
             end
-  message << 'people searched for bitcoin on Google Trends yesterday than last week'
+  message << 'people searched for bitcoin on Google yesterday than last week'
 
   to_json(message)
+end
+
+def data
+  # Connect to a server that retrieves Google Trend data for 'Bitcoin'
+  Net::HTTP.get(URI.parse("http://localhost:4000"))
+end
+
+def trend_diff(timeline)
+  trend_yesterday = timeline[-1]["value"][0]
+  trend_last_week = timeline[0]["value"][0]
+  trend_yesterday - trend_last_week
 end
 
 def to_json(message)
